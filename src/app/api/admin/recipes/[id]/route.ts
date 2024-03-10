3b4e1559e2d93aa7e18c9ac973123ec0a1ceedd8
Junichi Kako <147279646+JunichiKako@users.noreply.github.com>
2024-03-11 01:44:46 +0900

    更新処理の追加

--- src/app/api/admin/recipes/[id]/route.ts
+++ src/app/api/admin/recipes/[id]/route.ts
@@ -51,84 +51,128 @@ export const GET = async (
 };
 
 // PUTという命名にすることで、PUTリクエストの時にこの関数が呼ばれる
+
 export const PUT = async (
     request: NextRequest,
-    { params }: { params: { id: string } } // ここでリクエストパラメータを受け取る
+    { params }: { params: { id: string } }
 ) => {
     const { currentUser, error } = await getCurrentUser(request);
 
     if (error)
         return NextResponse.json({ status: error.message }, { status: 400 });
-    // paramsの中にidが入っているので、それを取り出す
-    const { id } = params;
 
-    // リクエストのbodyを取得
+    const { id } = params;
     const {
         title,
         thumbnailImageKey,
         categoryId,
-        materials,
-        howTos,
+        materials: clientMaterials,
+        howTos: clientHowTos,
     }: UpdateRecipeRequestBody = await request.json();
 
-    await prisma.material.deleteMany({
-        where: {
-            id: {
-                in: materials
-                    .map((material) => material.id)
-                    .filter((id): id is number => id !== undefined),
-            },
-        },
-    });
+    // トランザクションの開始
+    const transaction = await prisma.$transaction(async (prisma) => {
+        // 既存の材料（Materials）と手順（HowTos）の処理
+        const existingMaterialsIds = clientMaterials
+            .map((material) => material.id)
+            .filter((id): id is number => id !== undefined); // `id` が `number` 型のもののみを残す
 
-    await prisma.howTo.deleteMany({
-        where: {
-            id: {
-                in: howTos
-                    .map((howTo) => howTo.id)
-                    .filter((id): id is number => id !== undefined),
-            },
-        },
-    });
+        const existingHowTosIds = clientHowTos
+            .map((howTo) => howTo.id)
+            .filter((id): id is number => id !== undefined); // `id` が `number` 型のもののみを残す
 
-    try {
-        // idを指定して、Recipeを更新
-        const recipe = await prisma.recipe.update({
+        // 不要になった項目の削除
+        await prisma.material.deleteMany({
             where: {
-                id: parseInt(id),
+                AND: [
+                    { recipeId: parseInt(id) },
+                    { id: { notIn: existingMaterialsIds } },
+                ],
             },
+        });
+
+        await prisma.howTo.deleteMany({
+            where: {
+                AND: [
+                    { recipeId: parseInt(id) },
+                    { id: { notIn: existingHowTosIds } },
+                ],
+            },
+        });
+
+        // 更新が必要な既存項目の更新
+        await Promise.all(
+            clientMaterials
+                .filter((material) => material.id !== undefined)
+                .map((material) =>
+                    prisma.material.update({
+                        where: { id: material.id },
+                        data: {
+                            name: material.name,
+                            quantity: material.quantity,
+                        },
+                    })
+                )
+        );
+
+        await Promise.all(
+            clientHowTos
+                .filter((howTo) => howTo.id !== undefined)
+                .map((howTo) =>
+                    prisma.howTo.update({
+                        where: { id: howTo.id },
+                        data: { text: howTo.text, index: howTo.index },
+                    })
+                )
+        );
+
+        // 新しい項目の追加
+        const newMaterials = clientMaterials.filter(
+            (material) => material.id === undefined
+        );
+        const newHowTos = clientHowTos.filter(
+            (howTo) => howTo.id === undefined
+        );
+
+        await prisma.material.createMany({
+            data: newMaterials.map((material) => ({
+                name: material.name,
+                quantity: material.quantity,
+                recipeId: parseInt(id),
+            })),
+        });
+
+        await prisma.howTo.createMany({
+            data: newHowTos.map((howTo) => ({
+                text: howTo.text,
+                index: howTo.index,
+                recipeId: parseInt(id),
+            })),
+        });
+
+        // レシピの基本情報を更新
+        return prisma.recipe.update({
+            where: { id: parseInt(id) },
             data: {
                 title,
                 thumbnailImageKey,
                 categoryId,
-                materials: {
-                    create: materials.map((material) => ({
-                        name: material.name,
-                        quantity: material.quantity,
-                    })),
-                },
-                howTos: {
-                    create: howTos.map((howTo, index) => ({
-                        index: index,
-                        text: howTo.text,
-                    })),
-                },
+            },
+            include: {
+                materials: true,
+                howTos: true,
             },
         });
+    });
 
-        // レスポンスを返す
-        return NextResponse.json(
-            { status: "OK", recipe: recipe },
-            { status: 200 }
-        );
-    } catch (error) {
-        if (error instanceof Error)
-            return NextResponse.json(
-                { status: error.message },
-                { status: 400 }
-            );
-    }
+    // トランザクションの結果をレスポンスとして返す
+    return NextResponse.json(
+        { status: "OK", recipe: transaction },
+        { status: 200 }
+    );
 };
+
+// ...
 
 // DELETEという命名にすることで、DELETEリクエストの時にこの関数が呼ばれる
 export const DELETE = async (
