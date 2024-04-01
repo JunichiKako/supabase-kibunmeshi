2bbc9847a95348667b76630f146ed7d87142f954
Junichi Kako <147279646+JunichiKako@users.noreply.github.com>
2024-04-01 15:38:12 +0900

    useSWRの導入

---
+++ src/app/_hooks/useRecipes.ts
@@ -0,0 +1,53 @@
+import useSWR from "swr";
+import { useState, useEffect } from "react";
+import { Recipe } from "../types/recipe";
+import { supabase } from "../../utils/supabase";
+
+async function fetcher(url: string) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error("An error occurred while fetching the data.");
+    }
+    return response.json();
+}
+
+export const useRecipes = () => {
+    const { data, error, isValidating } = useSWR<Recipe[]>(
+        "/api/recipes",
+        fetcher
+    );
+    const [thumbnailUrls, setThumbnailUrls] = useState<{
+        [key: string]: string;
+    }>({});
+
+    useEffect(() => {
+        if (data) {
+            const fetchThumbnails = async () => {
+                const urls: { [key: string]: string } = {};
+                await Promise.all(
+                    data.map(async (recipe) => {
+                        if (recipe.thumbnailImageKey) {
+                            const { data: imageData } = await supabase.storage
+                                .from("recipe_thumbnail")
+                                .getPublicUrl(recipe.thumbnailImageKey);
+                            if (!error && imageData) {
+                                urls[recipe.id.toString()] =
+                                    imageData.publicUrl;
+                            }
+                        }
+                    })
+                );
+                setThumbnailUrls(urls);
+            };
+
+            fetchThumbnails();
+        }
+    }, [data]);
+
+    return {
+        recipes: data,
+        thumbnailUrls,
+        isLoading: isValidating,
+        error,
+    };
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,42 @@
 "use client";
 
-// Next.js 各ページはこの並びで記述する
+import { useRecipes } from "./_hooks/useRecipes";
+import Loading from "./_components/Loading/Loading";
 import Link from "next/link";
 import Image from "next/image";
-// React
-import React, { useState, useEffect } from "react";
-// Components
 import CategoryList from "./_components/CategoryList/CategoryList";
 import SearchRecipe from "./_components/SearchRecipe/SearchRecipe";
-import Loading from "./_components/Loading/Loading";
-// typeScript
-import { Recipe } from "./types/recipe";
-// CSS
 import styles from "./Home.module.css";
-import { supabase } from "../utils/supabase";
 
 export default function Home() {
-    const [recipeList, setRecipeList] = useState<Recipe[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<Error | null>(null);
-    const [thumbnailImageUrl, setThumbnailImageUrl] = useState<{
-        [key: number]: string;
-    }>({});
+    const { recipes, thumbnailUrls, isLoading, error } = useRecipes();
 
-    useEffect(() => {
-        async function fetchRecipesAndImages() {
-            setLoading(true);
-            try {
-                // レシピデータの取得
-                const response = await fetch("/api/recipes");
-                const data = await response.json();
-                if (!response.ok)
-                    throw new Error(
-                        data.message || "データの取得に失敗しました"
-                    );
-
-                setRecipeList(data);
-
-                // サムネイル画像URLの設定
-                const imageUrls: { [key: number]: string } = {};
-                await Promise.all(
-                    data.map(async (recipe:Recipe) => {
-                        if (recipe.thumbnailImageKey) {
-                            const { data } = await supabase.storage
-                                .from("recipe_thumbnail")
-                                .getPublicUrl(recipe.thumbnailImageKey);
-
-                            if (error) throw error;
-                            if (data.publicUrl)
-                                imageUrls[recipe.id] = data.publicUrl;
-                        }
-                    })
-                );
-
-                setThumbnailImageUrl(imageUrls);
-            } catch (error) {
-                setError(error as Error);
-            } finally {
-                setLoading(false); // データ取得後にローディングを終了
-            }
-        }
-
-        fetchRecipesAndImages();
-    }, []);
-
-
-    if (loading) {
-        return <Loading />;
-    }
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
+    if (isLoading) return <Loading />;
+    if (error) return <div>Error: {error.message}</div>;
 
     return (
         <>
             <SearchRecipe />
             <div className="new-content grid">
-                {recipeList.map((recipe) => (
-                    <div key={recipe.id} className="item">
-                        <Link href={`/recipe/${recipe.id}`}>
-                            {thumbnailImageUrl[recipe.id] ? (
-                                <Image
-                                    src={thumbnailImageUrl[recipe.id]}
-                                    alt={recipe.title}
-                                    width={300}
-                                    height={200}
-                                    style={{ objectFit: "cover" }}
-                                    loading="lazy"
-                                    rel="preload"
-                                />
-                            ) : (
-                                <div>No image available</div>
-                            )}
-                            <p>{recipe.title}</p>
-                        </Link>
-                    </div>
-                ))}
+                {recipes &&
+                    recipes.map((recipe) => (
+                        <div key={recipe.id} className="item">
+                            <Link href={`/recipe/${recipe.id}`}>
+                                {thumbnailUrls[recipe.id] ? (
+                                    <Image
+                                        src={thumbnailUrls[recipe.id]}
+                                        alt={recipe.title}
+                                        width={300}
+                                        height={200}
+                                        style={{ objectFit: "cover" }}
+                                    />
+                                ) : (
+                                    <div>レシピ画像が設定されていません</div>
+                                )}
+                                <p>{recipe.title}</p>
+                            </Link>
+                        </div>
+                    ))}
             </div>
 
             <div className={styles.recipe_all}>
